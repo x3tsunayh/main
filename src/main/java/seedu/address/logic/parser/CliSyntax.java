564d8d772691828fe4588070edd34d8b0a66da7a
Tan Yong He <tyhshana@hotmail.com>
2018-03-27 12:15:07 +0800

    Update CliSyntax.java

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -13,10 +13,10 @@ public class CliSyntax {
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
 
-    public static final Prefix PREFIX_EVENT_TITLE = new Prefix("et/");
-    public static final Prefix PREFIX_EVENT_DESCRIPTION = new Prefix("ed/");
-    public static final Prefix PREFIX_EVENT_LOCATION = new Prefix("el/");
-    public static final Prefix PREFIX_EVENT_DATETIME = new Prefix("edt/");
+    public static final Prefix PREFIX_TITLE = new Prefix("et/");
+    public static final Prefix PREFIX_DESCRIPTION = new Prefix("ed/");
+    public static final Prefix PREFIX_LOCATION = new Prefix("el/");
+    public static final Prefix PREFIX_DATETIME = new Prefix("edt/");
 
     public static final Prefix PREFIX_TASK_NAME = new Prefix("n/");
     public static final Prefix PREFIX_TASK_PRIORITY = new Prefix("p/");
