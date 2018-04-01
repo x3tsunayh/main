75ec6ff0822328829f4d435195185c8a8d626078
x3tsunayh <tyhshana@hotmail.com>
2018-04-01 17:26:42 +0800

    addiiton of new page for app UI

--- src/main/java/seedu/address/ui/StackOverflowWindow.java
+++ src/main/java/seedu/address/ui/StackOverflowWindow.java
@@ -1,4 +1,62 @@
 package seedu.address.ui;
 
-public class StackOverflowWindow {
+import java.util.logging.Logger;
+
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import seedu.address.commons.core.LogsCenter;
+
+/**
+ * Controller for a stackoverflow page
+ */
+public class StackOverflowWindow extends UiPart<Stage> {
+
+    public static final String stackOverflowUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+    private static final Logger logger = LogsCenter.getLogger(StackOverflowWindow.class);
+    private static final String FXML = "StackOverflowWindow.fxml";
+
+    @FXML
+    private WebView browser;
+
+    /**
+     * Creates a new StackOverflowWindow.
+     *
+     * @param root Stage to use as the root of the StackOverflowWindow.
+     */
+    public StackOverflowWindow(Stage root) {
+        super(FXML, root);
+        browser.getEngine().load(stackOverflowUrl);
+    }
+
+    /**
+     * Creates a new StackOverflowWindow.
+     */
+    public StackOverflowWindow() {
+        this(new Stage());
+    }
+
+    /**
+     * Shows the stackoverflow window.
+     * @throws IllegalStateException
+     * <ul>
+     *     <li>
+     *         if this method is called on a thread other than the JavaFX Application Thread.
+     *     </li>
+     *     <li>
+     *         if this method is called during animation or layout processing.
+     *     </li>
+     *     <li>
+     *         if this method is called on the primary stage.
+     *     </li>
+     *     <li>
+     *         if {@code dialogStage} is already showing.
+     *     </li>
+     * </ul>
+     */
+    public void show() {
+        logger.fine("Showing the stackoverflow page.");
+        getRoot().show();
+    }
 }
