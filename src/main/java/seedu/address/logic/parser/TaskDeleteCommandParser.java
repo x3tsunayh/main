34d5d3a9e054c6d2ad551beafd0d76bb41415111
CYX28 <chanyx17@gmail.com>
2018-04-01 22:12:26 +0800

    Implement task-delete command

---
+++ src/main/java/seedu/address/logic/parser/TaskDeleteCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.TaskDeleteCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new TaskDeleteCommand object.
+ */
+public class TaskDeleteCommandParser implements Parser<TaskDeleteCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the TaskDeleteCommand
+     * and returns a TaskDeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform to the expected format
+     */
+    public TaskDeleteCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new TaskDeleteCommand(index);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, TaskDeleteCommand.MESSAGE_USAGE));
+        }
+    }
+
+}
