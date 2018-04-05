bcd3d48f83f359d547e7907d3770f022a2aae5c2
Unknown <jill.wee13@gmail.com>
2018-04-05 08:02:42 +0800

    Update find command such that able to find by name and tag

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,24 +1,28 @@
 package seedu.address.logic.commands;
 
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import java.util.function.Predicate;
+
+import seedu.address.model.person.Person;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Finds and lists all persons in address book whose name or tags contains any of the keywords or partial keywords.
+ * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
     public static final String COMMAND_ALIAS = "f";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds persons whose names or tags contain any of "
+            + "the keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "By Name Parameters: n/KEYWORD [MORE_KEYWORDS]... \n"
+            + "By Tag Parameters: t/KEYWORD [MORE_KEYWORDS].. \n"
+            + "Example: " + COMMAND_WORD + " n/alice bob charlie \n"
+            + "Example: " + COMMAND_WORD + " t/friends";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private Predicate<Person> predicate;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
+    public FindCommand(Predicate<Person> predicate) {
         this.predicate = predicate;
     }
 
