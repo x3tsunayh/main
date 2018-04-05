b20f4fcea10cb899ab64ca251f5de4515a6a2d13
Unknown <jill.wee13@gmail.com>
2018-04-05 08:04:05 +0800

    Test: Update test cases for Find Command

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -6,9 +6,16 @@ import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalEvents.getTypicalEventBook;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
 import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.DANIEL;
 import static seedu.address.testutil.TypicalPersons.ELLE;
 import static seedu.address.testutil.TypicalPersons.FIONA;
+import static seedu.address.testutil.TypicalPersons.GEORGE;
+import static seedu.address.testutil.TypicalPersons.HARRY;
+import static seedu.address.testutil.TypicalPersons.IAN;
+import static seedu.address.testutil.TypicalPersons.KEITH;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -24,6 +31,7 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.model.tag.TagContainsKeywordsPredicate;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code FindCommand}.
@@ -61,23 +69,115 @@ public class FindCommandTest {
     @Test
     public void execute_zeroKeywords_noPersonFound() {
         String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
-        FindCommand command = prepareCommand(" ");
+        FindCommand command = prepareNameCommand(" ");
         assertCommandSuccess(command, expectedMessage, Collections.emptyList());
     }
 
     @Test
     public void execute_multipleKeywords_multiplePersonsFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
-        FindCommand command = prepareCommand("Kurz Elle Kunz");
-        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, ELLE, FIONA));
+        // multiple name keywords
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 4);
+        FindCommand command = prepareNameCommand("Kurz Elle Kunz");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, ELLE, FIONA, IAN));
+
+        // multiple partial name keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 5);
+        command = prepareNameCommand("Ku El");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, DANIEL, ELLE, FIONA, IAN));
+
+        // multiple mixed-case name keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 4);
+        command = prepareNameCommand("KuRz ElLe KuNz");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, ELLE, FIONA, IAN));
+
+        // multiple mixed-case partial name keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 2);
+        command = prepareNameCommand("KuN ElL");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(ELLE, FIONA));
+
+        // multiple tag keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
+        command = prepareTagCommand("colleagues classmates");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(HARRY, IAN, KEITH));
+
+        // multiple partial tag keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 4);
+        command = prepareTagCommand("c owesM");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(BENSON, HARRY, IAN, KEITH));
+
+        // multiple mixed-case tag keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
+        command = prepareTagCommand("colLeagUes clasSMates");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(HARRY, IAN, KEITH));
+
+        // multiple mixed-case partial tag keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
+        command = prepareTagCommand("colL owESM");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(BENSON, HARRY, IAN));
+    }
+
+    @Test
+    public void execute_singleKeywords_multiplePersonsFound() {
+        // name keywords
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 2);
+        FindCommand command = prepareNameCommand("Kurz");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, IAN));
+
+        //  partial name keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
+        command = prepareNameCommand("Ku");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, FIONA, IAN));
+
+        // mixed-case name keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 2);
+        command = prepareNameCommand("KuRz");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, IAN));
+
+        //  mixed-case partial name keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
+        command = prepareNameCommand("kU");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, FIONA, IAN));
+
+
+
+        // tag keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 7);
+        command = prepareTagCommand("friends");
+        assertCommandSuccess(command, expectedMessage,
+                Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
+
+        //  partial tag keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 2);
+        command = prepareTagCommand("class");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(IAN, KEITH));
+
+        //  mixed-case tag keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 2);
+        command = prepareTagCommand("oweSMoney");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(BENSON, IAN));
+
+        //  mixed-case partial tag keywords
+        expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 2);
+        command = prepareTagCommand("owesM");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(BENSON, IAN));
     }
 
     /**
-     * Parses {@code userInput} into a {@code FindCommand}.
+     * Parses {@code userInput} into a {@code FindCommand} for a name search.
      */
-    private FindCommand prepareCommand(String userInput) {
+    private FindCommand prepareNameCommand(String userInput) {
         FindCommand command =
                 new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+"))));
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code FindCommand} for a tag search.
+     */
+    private FindCommand prepareTagCommand(String userInput) {
+        FindCommand command =
+                new FindCommand(new TagContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+"))));
         command.setData(model, new CommandHistory(), new UndoRedoStack());
         return command;
     }
--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.tag.TagContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -23,12 +24,20 @@ public class FindCommandParserTest {
     @Test
     public void parse_validArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
-        FindCommand expectedFindCommand =
+        FindCommand expectedFindNameCommand =
                 new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
-        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+        assertParseSuccess(parser, "n/Alice Bob", expectedFindNameCommand);
 
         // multiple whitespaces between keywords
-        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+        assertParseSuccess(parser, "n/Alice \n \t Bob  \t", expectedFindNameCommand);
+
+        FindCommand expectedFindTagCommand =
+                new FindCommand(new TagContainsKeywordsPredicate(Arrays.asList("owesMoney", "friends")));
+
+        assertParseSuccess(parser, "t/owesMoney friends", expectedFindTagCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, "t/owesMoney\t friends  \t", expectedFindTagCommand);
     }
 
 }
