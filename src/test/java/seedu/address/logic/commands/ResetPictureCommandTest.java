17cc792721189ca381e59b4e33afd48ce79314ce
dezhanglee <dzhanglee@yahoo.com>
2018-04-11 19:41:31 +0800

    tests

---
+++ src/test/java/seedu/address/logic/commands/ResetPictureCommandTest.java
@@ -0,0 +1,105 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalEvents.getTypicalEventBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+
+//@@author dezhanglee
+public class ResetPictureCommandTest {
+
+    private ModelManager model = new ModelManager(getTypicalAddressBook(), getTypicalEventBook(), new UserPrefs());
+    private Index index = INDEX_FIRST_PERSON;
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() throws Exception {
+
+
+        Person toUpdatePerson = model.getFilteredPersonList().get(index.getZeroBased());
+        Person updatedPerson = new Person(toUpdatePerson);
+
+        updatedPerson.resetPicture();
+        ResetPictureCommand resetPictureCommand = prepareCommand(index);
+        String expectedMessage = String.format(ResetPictureCommand.MESSAGE_EDIT_PERSON_SUCCESS, index.getOneBased());
+        Model expectedModel = new ModelManager(model.getAddressBook(), getTypicalEventBook(), new UserPrefs());
+        expectedModel.updatePerson(model.getFilteredPersonList().get(0), updatedPerson);
+
+        assertCommandSuccess(resetPictureCommand, model, expectedMessage, expectedModel);
+
+    }
+
+    @Test
+    public void execute_validIndexFilteredList_success() throws Exception {
+
+        showPersonAtIndex(model, index);
+
+        Person toUpdatePerson = model.getFilteredPersonList().get(index.getZeroBased());
+        Person updatedPerson = new Person(toUpdatePerson);
+
+        updatedPerson.resetPicture();
+        ResetPictureCommand resetPictureCommand = prepareCommand(index);
+        String expectedMessage = String.format(ResetPictureCommand.MESSAGE_EDIT_PERSON_SUCCESS, index.getOneBased());
+        Model expectedModel = new ModelManager(model.getAddressBook(), getTypicalEventBook(), new UserPrefs());
+        expectedModel.updatePerson(model.getFilteredPersonList().get(0), updatedPerson);
+
+        assertCommandSuccess(resetPictureCommand, model, expectedMessage, expectedModel);
+
+    }
+
+    @Test
+    public void execute_filteredListInvalidIndex_failure() throws Exception {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        ResetPictureCommand resetPictureCommand = prepareCommand(outOfBoundIndex);
+
+        assertCommandFailure(resetPictureCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() throws Exception {
+        ResetPictureCommand resetPicCommandFirst = prepareCommand(INDEX_FIRST_PERSON);
+        ResetPictureCommand resetPicCommandSecond = prepareCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertTrue(resetPicCommandFirst.equals(resetPicCommandFirst));
+
+        // same values -> returns true
+        ResetPictureCommand resetPicCommandFirstCopy = prepareCommand(INDEX_FIRST_PERSON);
+        assertTrue(resetPicCommandFirstCopy.equals(resetPicCommandFirst));
+
+        // different types -> returns false
+        assertFalse(resetPicCommandFirst.equals(1));
+
+        // null -> returns false
+        assertFalse(resetPicCommandFirst.equals(null));
+
+        // different indexes -> returns false
+        assertFalse(resetPicCommandSecond.equals(resetPicCommandFirst));
+
+    }
+
+
+    /**
+     * Returns an {@code ResetPictureCommand} with parameters {@code index}
+     */
+    private ResetPictureCommand prepareCommand(Index index) {
+        ResetPictureCommand resetPictureCommand = new ResetPictureCommand(index);
+        resetPictureCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return resetPictureCommand;
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/ResetPictureCommandParserTest.java
@@ -0,0 +1,51 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ResetPictureCommand;
+
+//@@author dezhanglee
+public class ResetPictureCommandParserTest {
+
+    private static final String MESSAGE_INVALID_FORMAT =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, ResetPictureCommand.MESSAGE_USAGE);
+
+    private ResetPictureCommandParser parser = new ResetPictureCommandParser();
+
+    @Test
+    public void parse_missingParts_failure() {
+
+        //no index specified
+        assertParseFailure(parser);
+    }
+
+    @Test
+    public void parse_invalidInput_failure() {
+        // negative index
+        assertParseFailure(parser, "-5", MESSAGE_INVALID_FORMAT);
+
+        // zero index
+        assertParseFailure(parser, "0", MESSAGE_INVALID_FORMAT);
+
+        // multiple repeated index
+        assertParseFailure(parser, "1 1", MESSAGE_INVALID_FORMAT);
+
+        // multiple distinct index
+        assertParseFailure(parser, "1 2", MESSAGE_INVALID_FORMAT);
+
+
+        // invalid arguments being parsed as preamble
+        assertParseFailure(parser, "1 some random string", MESSAGE_INVALID_FORMAT);
+
+    }
+
+
+
+
+}
